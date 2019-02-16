2ce918cf875f78474a1e1e03f6032825cdfc5190
Aleksandr YUrkovskiy <godlikedesigner@gmail.com>
2019-02-16 23:33:39 +0500

    add header component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
+import ApolloClient from 'apollo-boost';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import App from './components/app';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+    uri: 'https://damp-wildwood-52429.herokuapp.com/',
+    fetchOptions: {
+        credentials: 'include'
+    },
+    request: operation => {
+        const token = localStorage.getItem('token');
+        operation.setContext({
+            headers: {
+                authorization: `Bearer ${token}`
+            }
+        })
+    },
+    onError({ networkError }) {
+        console.log(networkError);
+    }
+});
 
 ReactDOM.render(
-<Router>
-    <App />
-</Router>
+    <ApolloProvider client={client}>
+        <Provider store={store}>
+            <Router>
+                <App />
+            </Router>
+        </Provider>
+    </ApolloProvider>
 , document.getElementById('root'));
