3e09473292f4a827d4e07de18d1216f8d0c10305
Aleksandr YUrkovskiy <godlikedesigner@gmail.com>
2019-02-19 13:49:46 +0500

    fix bug router with redux

--- src/index.js
+++ src/index.js
@@ -4,27 +4,40 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
 import { Provider } from 'react-redux';
 import store from './store';
+import 'semantic-ui-css/semantic.min.css';
 
 import App from './components/app';
 import { ApolloProvider } from 'react-apollo';
 
-const client = new ApolloClient({
-    uri: 'https://damp-wildwood-52429.herokuapp.com/',
-    fetchOptions: {
-        credentials: 'include'
-    },
-    request: operation => {
-        const token = localStorage.getItem('token');
-        operation.setContext({
-            headers: {
-                authorization: `Bearer ${token}`
+const authClient = (token) => {
+    if(token !== null) {
+        return new ApolloClient({
+            uri: 'https://damp-wildwood-52429.herokuapp.com/',
+            fetchOptions: {
+                credentials: 'include'
+            },
+            request: operation => {
+                operation.setContext({
+                    headers: {
+                        authorization: `Bearer ${token}`
+                    }
+                })
+            },
+            onError({ networkError }) {
+                console.log(networkError);
             }
-        })
-    },
-    onError({ networkError }) {
-        console.log(networkError);
+        });
+    } else {
+        return new ApolloClient({
+            uri: 'https://damp-wildwood-52429.herokuapp.com/',
+            fetchOptions: {
+                credentials: 'include'
+            }
+        });
     }
-});
+};
+
+const client = authClient(localStorage.getItem('token'));
 
 ReactDOM.render(
     <ApolloProvider client={client}>
